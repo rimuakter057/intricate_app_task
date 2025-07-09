174503cffafd5b5b58f064f3fb892ffe7a633977
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-07-09 18:49:39 +0600

    ui

--- lib/features/assignment_one/ui/screens/on_boarding_screen.dart
+++ lib/features/assignment_one/ui/screens/on_boarding_screen.dart
@@ -21,49 +21,51 @@ class OnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Padding(
-        padding:  EdgeInsets.all(24.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: (){
-              Get.toNamed(ProductScreen.name);
-            }, child: Text("assignment-2")),
-            SizedBox(height: 15.h),
-            TextButton(onPressed: (){
-              Get.toNamed(GetApiScreen.name);
-            }, child: Text("assignment-3")),
-            SvgPicture.asset(AssetsPath.onboardingImage, width: 234.w,height: 220.h,),
-            SizedBox(height: 32.h),
-        Text(
-        'Your Journey to a Better\nMind Starts Here.',
-        textAlign: TextAlign.center,
-        style: AppTextStyles.headlineTextStyle,
-      ),
-
-      SizedBox(height: 8.h),
-
-            Text(
-              'We’re here to help you build daily routines\nthat restore balance, joy, and strength —\none step at a time.',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.inter(
-                fontSize: 14.sp,       // screen_utils এর responsive font size
-                fontWeight: FontWeight.normal, // Regular weight
-                color: AppColors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:  EdgeInsets.all(24.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: (){
+                Get.toNamed(ProductScreen.name);
+              }, child: Text("assignment-2")),
+              SizedBox(height: 15.h),
+              TextButton(onPressed: (){
+                Get.toNamed(GetApiScreen.name);
+              }, child: Text("assignment-3")),
+              Image.asset("assets/images/on_boarding_image.png", width: 234.w,height: 220.h,),
+              SizedBox(height: 32.h),
+          Text(
+          'Your Journey to a Better\nMind Starts Here.',
+          textAlign: TextAlign.center,
+          style: AppTextStyles.headlineTextStyle,
+        ),
+        
+        SizedBox(height: 8.h),
+        
+              Text(
+                'We’re here to help you build daily routines\nthat restore balance, joy, and strength —\none step at a time.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.inter(
+                  fontSize: 14.sp,       // screen_utils এর responsive font size
+                  fontWeight: FontWeight.normal, // Regular weight
+                  color: AppColors.black,
+                ),
               ),
-            ),
-            SizedBox(height: 64.h),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, StepOneScreen.name);
-              },
-              child:Text(
-                'Get Started – Free 3-Day Trial',
-                style: AppTextStyles.buttonTextStyle,
-              ),
-            )
-          ],
+              SizedBox(height: 64.h),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, StepOneScreen.name);
+                },
+                child:Text(
+                  'Get Started – Free 3-Day Trial',
+                  style: AppTextStyles.buttonTextStyle,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
